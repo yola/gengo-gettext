d375c58184a1413e7a7b9b9afc57ebb0c68d526d
Stefano Rivera <stefano@yola.com>
2015-01-07 13:59:53 -0800

    Tests coverage of automated checks

--- tests/test_gengogettext.py
+++ tests/test_gengogettext.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+import collections
 import os
 import unittest
 
@@ -31,3 +33,99 @@ class TestGengoGettext(unittest.TestCase):
         self.assertEqual(request.call_count, 1)
         called_url = request.call_args_list[0][0][1]
         self.assertIn('translate/jobs/', called_url)
+
+
+class TestTranslationChecks(unittest.TestCase):
+
+    def check_translation(self, source, translation):
+        Job = collections.namedtuple('Job', ('source', 'translation'))
+        job = Job(source, translation)
+        return gengogettext.check_translation(job)
+
+    def test_only_text(self):
+        self.assertTrue(self.check_translation('Hello', 'Ciao'))
+
+    def test_empty_translation(self):
+        self.assertFalse(self.check_translation(
+            'Hello',
+            ' '))
+
+    def test_correct_html(self):
+        self.assertTrue(self.check_translation(
+            'Click <a>this</a> link',
+            'Ciicca <a>questo</a> link'))
+
+    def test_missing_html(self):
+        self.assertFalse(self.check_translation(
+            'Click <a>this</a> link',
+            'Ciicca <a>questo link'))
+
+    def test_translated_html(self):
+        self.assertFalse(self.check_translation(
+            'Click <a>this</a> link',
+            'Ciicca <un>questo</un> link'))
+
+    def test_broken_html(self):
+        self.assertFalse(self.check_translation(
+            'Click <a class="blue">this</a> link',
+            'Ciicca <a class=\'blue">questo</a> link'))
+
+    def test_correct_substitution(self):
+        self.assertTrue(self.check_translation(
+            'Only $%s',
+            'Solo $%s'))
+
+    def test_missing_substitution(self):
+        self.assertFalse(self.check_translation(
+            'Only $%s',
+            'Solo $s'))
+
+    def test_correct_named_substitution(self):
+        self.assertTrue(self.check_translation(
+            'Only $%(price)s',
+            'Solo $%(price)s'))
+
+    def test_missing_named_substitution(self):
+        self.assertFalse(self.check_translation(
+            'Only $%(price)s',
+            'Solo $s'))
+
+    def test_translated_named_substitution(self):
+        self.assertFalse(self.check_translation(
+            'Only $%(price)s',
+            'Solo $%(prezzo)s'))
+
+    def test_missing_dollar_substitution(self):
+        self.assertFalse(self.check_translation(
+            'Only $%1$s',
+            'Solo $%1s'))
+
+    def test_unescaped_percent(self):
+        self.assertFalse(self.check_translation(
+            '2%% discount',
+            'Sconto del 2%'))
+
+    def test_correct_percent_escaping(self):
+        self.assertFalse(self.check_translation(
+            '2%% discount',
+            'Sconto del 2%%'))
+
+    def test_correct_entity(self):
+        self.assertTrue(self.check_translation(
+            'House &amp; Garden',
+            'Casa &amp; Giardino'))
+
+    def test_translated_entity(self):
+        self.assertFalse(self.check_translation(
+            'House &amp; Garden',
+            'Casa &amplificatore; Giardino'))
+
+    def test_spaced_entity(self):
+        self.assertFalse(self.check_translation(
+            'House &amp; Garden',
+            'Casa & amp ; Giardino'))
+
+    def test_full_width_semicolon_entity(self):
+        self.assertFalse(self.check_translation(
+            'House &amp; Garden',
+            'Casa &amp； Giardino'))
